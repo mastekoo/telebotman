9acc89127adeb75d1134794018e38192c79e7a45
Sviatoslav <mstkvsk@gmail.com>
2023-02-06 21:16:53 +0600

    add port validate

--- lib/present/bot_settings.dart
+++ lib/present/bot_settings.dart
@@ -18,62 +18,77 @@ class _NewRequestState extends State<NewRequest> {
   String _port = '';
   String? baseUrl = dotenv.env['BASE_URL'];
   String? error;
+  var items = ['8443', '80', '88', '443'];
+  String defaultPort = '8443';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     _hostController.text = _host;
     _portController.text = _port;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Enter bot token'),
-            controller: _tokenController,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Enter your Host'),
-            controller: _hostController,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Enter your port'),
-            controller: _portController,
-          ),
-        ),
-        ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                foregroundColor: MaterialStateProperty.all(Colors.blueGrey)),
-            onPressed: getWebhook,
-            child: const Text('getWebhook')),
-        ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                foregroundColor: MaterialStateProperty.all(Colors.blueGrey)),
-            onPressed: setWebhook,
-            child: const Text('setWebhook')),
-        Text(_responseBody)
-      ],
-    );
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Enter bot token'),
+                controller: _tokenController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Enter your Host'),
+                controller: _hostController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                validator: (value) {
+                  if (!items.contains(value)) {
+                    _portController.text = defaultPort;
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your port, default is 8443'),
+                controller: _portController,
+              ),
+            ),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    foregroundColor:
+                        MaterialStateProperty.all(Colors.blueGrey)),
+                onPressed: getWebhook,
+                child: const Text('getWebhook')),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    foregroundColor:
+                        MaterialStateProperty.all(Colors.blueGrey)),
+                onPressed: setWebhook,
+                child: const Text('setWebhook')),
+            Text(_responseBody)
+          ],
+        ));
   }
 
   void setWebhook() async {
-    var host = _hostController.text;
-    var token = _tokenController.text;
-    var port = _portController.text;
-    var url = '$baseUrl/bot$token/setWebhook?url=$host:$port';
-    var response = await http.get(Uri.parse(url));
-    setState(() => {_responseBody = response.body});
+    if (_formKey.currentState!.validate()) {
+      var host = _hostController.text;
+      var token = _tokenController.text;
+      var port = _portController.text;
+      var url = '$baseUrl/bot$token/setWebhook?url=$host:$port';
+      var response = await http.get(Uri.parse(url));
+      setState(() => {_responseBody = response.body});
+    }
   }
 
   void getWebhook() async {
